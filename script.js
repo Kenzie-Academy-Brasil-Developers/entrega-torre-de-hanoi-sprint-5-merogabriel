bbc3ea32a59ccd2f1a14cc227fa6b61b343c9def
Gabriel Mero <og.gabrielmero@gmail.com>
2021-09-28 20:27:27 -0300

    Appends nas torres adicionado

---
+++ script.js
@@ -0,0 +1,45 @@
+const t1 = document.getElementById('t1')
+const t2 = document.getElementById('t2')
+const t3 = document.getElementById('t3')
+let discoAtual = undefined
+let modo = 'modo torre'
+
+t1.addEventListener("click", function () {
+    if (modo === 'modo torre') {
+        discoAtual = t1.lastElementChild
+        console.log(modo)
+        modo = 'modo disco'
+    } else if (modo === 'modo disco') {
+        t1.appendChild(discoAtual)
+        discoAtual = undefined
+        console.log(modo)
+        modo = 'modo torre'
+    }
+})
+
+t2.addEventListener("click", function () {
+    if (modo === 'modo torre') {
+        discoAtual = t2.lastElementChild
+        console.log(modo)
+        modo = 'modo disco'
+    } else if (modo === 'modo disco') {
+        t2.appendChild(discoAtual)
+        discoAtual = undefined
+        console.log(modo)
+        modo = 'modo torre'
+    }
+})
+
+t3.addEventListener("click", function () {
+    if (modo === 'modo torre') {
+        discoAtual = t3.lastElementChild
+        console.log(modo)
+        modo = 'modo disco'
+    } else if (modo === 'modo disco') {
+        t3.appendChild(discoAtual)
+        discoAtual = undefined
+        console.log(modo)
+        modo = 'modo torre'
+    }
+})
+
